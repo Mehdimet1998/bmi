08b7eb3feace6dcf9d9fa2920c1f5675dd392815
sdaddf <72619476+Mehdimet1998@users.noreply.github.com>
2024-04-22 22:19:12 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@
 import { connect } from 'cloudflare:sockets';
 
 // 建议修改为自己的 UUID
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'dcbad18a-5d82-4d9e-85a4-32fe60639359';
 
 // 生成配置文件的 CF 优选 IP
 const bestCFIP = "www.gov.se"
